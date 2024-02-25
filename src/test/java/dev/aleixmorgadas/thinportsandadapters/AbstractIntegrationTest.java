b8815293561e343a5a7698fd3497baad213f30e8
Aleix Morgadas <hello@aleixmorgadas.dev>
2024-02-25 14:52:11 +0100

    refactor-test: made the AbstractIntegrationTest a singleton

--- src/test/java/dev/aleixmorgadas/thinportsandadapters/AbstractIntegrationTest.java
+++ src/test/java/dev/aleixmorgadas/thinportsandadapters/AbstractIntegrationTest.java
@@ -1,27 +1,32 @@
 package dev.aleixmorgadas.thinportsandadapters;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 
 @SpringBootTest
-@Testcontainers
 @AutoConfigureMockMvc
 public class AbstractIntegrationTest {
     @Autowired
     protected MockMvc mockMvc;
 
-    @Container
-    @ServiceConnection
-    static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
 
     static {
-        postgreSQLContainer.start();
+        postgres.start();
+    }
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
     }
 }
